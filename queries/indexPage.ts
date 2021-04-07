378a49bea3c26d89b447ec1602d126437a3bf2e7
Sean Aguinaga <aguinaga.sean@gmail.com>
2021-04-07 09:58:44 -0700

    order_by does not seem to work?

--- queries/indexPage.ts
+++ queries/indexPage.ts
@@ -2,7 +2,7 @@ import { graphql } from "react-relay";
 
 export default graphql`
   query indexPageQuery {
-    notes_app_notes(order_by: { updated_at: asc_nulls_last }) {
+    notes_app_notes(order_by: { updated_at: asc_nulls_first }) {
       id
       title
       text
